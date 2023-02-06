48a6eca4566c7ff7f87f0b5460760afdc8d508b4
KingDundee <118585837+KingDundee@users.noreply.github.com>
2023-02-05 22:51:32 -0800

    Refactor logic for calculator - step1

--- mainScript.js
+++ mainScript.js
@@ -16,219 +16,194 @@ const divide = function(a, b) {
   return a / b;
 }
 
-// function to call one of the above functions
+// function to call the basic math operator functions
 
 const operate = function(operator, numOne, numTwo) {
   return operator(numOne, numTwo);
 }
-
-// functions to populate the display
-
-const operandButtons = document.querySelectorAll('button.operand');
-const operatorButtons = document.querySelectorAll('button.operator');
-const decimalButton = document.querySelector('button.float');
-const deleteButton = document.querySelector('button.backspace');
-const clearButton = document.querySelector('button.clear-entry');
-
-const displayLine1 = document.querySelector('p.disp-line1');
-const displayLine2 = document.querySelector('p.disp-line2');
-
-let numberLast = null; // NUMBER last entered
-
-let lastOperator = ""; // last OPERATOR selected
-
-let lastOperand = "";
-let bufferOperand = "";
-
-const displayOperand = function(e) {
+//---------------------------------------------------------------------------------
+const expressionDisplay = document.querySelector('[data-display = "expression"]');
+const indicatorDisplay = document.querySelector('[data-display = "indicator"]');
+//---------------------------------------------------------------------------------
+const operandButtons = document.querySelectorAll('[data-operand]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+//---------------------------------------------------------------------------------
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+//---------------------------------------------------------------------------------
+const calculator = {
+  lastInput: '',
+  currentInput: '',
+  currentOperator: '',
+  get dispLine1() {
+    return expressionDisplay.textContent;
+  },
+  set dispLine1(value) {
+    expressionDisplay.textContent = value;
+  },
+  get dispLine2() {
+    return indicatorDisplay.textContent;
+  },
+  set dispLine2(value) {
+    indicatorDisplay.textContent = value;
+  },
+  firstOperand: '',
+  secondOperand: '',
+  result: '',
+};
+//---------------------------------------------------------------------------------
+const setDisplay = function(inputValue) {
+  calculator.dispLine1 += inputValue;
+  calculator.dispLine2 = inputValue;
+};
+//---------------------------------------------------------------------------------
+const getInput = function(e) {
+  calculator.lastInput = calculator.currentInput; // capture previous input value before getting new input value
   switch (e.target.id) {
-    case "zero":
-      numberLast = '0';
-      displayLine2.textContent = numberLast; // indicator
-
-      bufferOperand += numberLast; // operand buffer
-      displayLine1.textContent += numberLast;
+    case 'zero':
+      calculator['currentInput'] = '0';
+      setDisplay(calculator.currentInput);
       break;
-    case "one":
-      numberLast = '1';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'one':
+      calculator['currentInput'] = '1';
+      setDisplay(calculator.currentInput);
       break;
-    case "two":
-      numberLast = '2';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'two':
+      calculator['currentInput'] = '2';
+      setDisplay(calculator.currentInput);
       break;
-    case "three":
-      numberLast = '3';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'three':
+      calculator['currentInput'] = '3';
+      setDisplay(calculator.currentInput);
       break;
-    case "four":
-      numberLast = '4';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'four':
+      calculator['currentInput'] = '4';
+      setDisplay(calculator.currentInput);
       break;
-    case "five":
-      numberLast = '5';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'five':
+      calculator['currentInput'] = '5';
+      setDisplay(calculator.currentInput);
       break;
-    case "six":
-      numberLast = '6';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'six':
+      calculator['currentInput'] = '6';
+      setDisplay(calculator.currentInput);
       break;
-    case "seven":
-      numberLast = '7';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'seven':
+      calculator['currentInput'] = '7';
+      setDisplay(calculator.currentInput);
       break;
-    case "eight":
-      numberLast = '8';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'eight':
+      calculator['currentInput'] = '8';
+      setDisplay(calculator.currentInput);
       break;
-    case "nine":
-      numberLast = '9';
-      displayLine2.textContent = numberLast;
-
-      bufferOperand += numberLast;
-      displayLine1.textContent += numberLast;
+    case 'nine':
+      calculator['currentInput'] = '9';
+      setDisplay(calculator.currentInput);
+      break;
+    case 'decimal':
+      calculator['currentInput'] = '.';
+      setDisplay(calculator.currentInput);
       break;
   }
 };
-
+//---------------------------------------------------------------------------------
 const setOperator = function(e) {
+  calculator.lastInput = calculator.currentInput; // capture previous input value before getting new input value
   switch (e.target.id) {
-    case "add":
+    case 'add':
+      calculator['currentInput'] = '+';
+      setDisplay(calculator.currentInput);
+      calculator['currentOperator'] = '+';
+      break;
+    case 'subtract':
+      calculator['currentInput'] = '-';
+      setDisplay(calculator.currentInput);
+      calculator['currentOperator'] = '-';
+      break;
+    case 'multiply':
+      calculator['currentInput'] = '✕';
+      setDisplay(calculator.currentInput);
+      calculator['currentOperator'] = '✕';
+      break;
+    case 'divide':
+      calculator['currentInput'] = '÷';
+      setDisplay(calculator.currentInput);
+      calculator['currentOperator'] = '÷';
+      break;
+    case 'equals':
+      calculator['currentInput'] = '=';
+      setDisplay(calculator.currentInput);
+      // calculator['currentOperator'] = '='; // LEAVE THIS OUT FOR NOW
 
-      if (lastOperand !== "" && bufferOperand !== "" && lastOperator !== "") {
-        displayLine1.textContent = operate(add, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-        // if both operands full, call calc function before adding
-      }
-      else if (lastOperand === "") {
-        lastOperand = bufferOperand; // dump buffer for next entry
-        bufferOperand = ""; // reset buffer
-      }
-
-      lastOperator = 'add';
-      displayLine1.textContent += '+';
+      getResult();
 
       break;
-    case "subtract":
-
-      if (lastOperand !== "" && bufferOperand !== "" && lastOperator !== "") {
-        displayLine1.textContent = operate(subtract, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-        // if both operands full, call calc function before subtracting
-      }
-      else if (lastOperand === "") {
-        lastOperand = bufferOperand; // dump buffer for next entry
-        bufferOperand = ""; // reset buffer
-      }
-
-      lastOperator = 'subtract';
-      displayLine1.textContent += '-';
-
+  } // need a way to flush out the current operator once an equals operation is carried out
+};
+//---------------------------------------------------------------------------------
+const clearDisplay = function() {
+  calculator.dispLine1 = '';
+  calculator.dispLine2 = '';
+  calculator.lastInput = '';
+  calculator.currentInput = '';
+  calculator.currentOperator = '';
+  calculator.firstOperand = '';
+  calculator.secondOperand = '';
+  calculator.result = '';
+};
+//---------------------------------------------------------------------------------
+const getResult = function() {
+  parseExpression(calculator.dispLine1);
+  let operandA = +calculator.firstOperand;
+  let operandB = +calculator.secondOperand;
+  switch (calculator.currentOperator) {
+    case '+':
+      calculator.dispLine1 = operate(add, operandA, operandB);
       break;
-    case "multiply":
-      
-      if (lastOperand !== "" && bufferOperand !== "" && lastOperator !== "") {
-        displayLine1.textContent = operate(multiply, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-        // if both operands full, call calc function before multiplying
-      }
-      else if (lastOperand === "") {
-        lastOperand = bufferOperand; // dump buffer for next entry
-        bufferOperand = ""; // reset buffer
-      }
-
-      lastOperator = 'multiply';
-      displayLine1.textContent += '×';
-
+    case '-':
+      calculator.dispLine1 = operate(subtract, operandA, operandB);
       break;
-    case "divide":
-
-      if (lastOperand !== "" && bufferOperand !== "" && lastOperator !== "") {
-        displayLine1.textContent = operate(divide, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-        // if both operands full, call calc function before dividing
-      }
-      else if (lastOperand === "") {
-        lastOperand = bufferOperand; // dump buffer for next entry
-        bufferOperand = ""; // reset buffer
-      }
-      lastOperator = 'divide';
-      displayLine1.textContent += '÷';
-
+    case '✕':
+      calculator.dispLine1 = operate(multiply, operandA, operandB);
       break;
-    case "equals":
-
-      if (bufferOperand === "") {
-        displayLine2.textContent = 'Enter next operand!';
-        break;
-      }
-      if (lastOperator === "") {
-        displayLine2.textContent = 'Enter next operator!';
-        break;
-      }
-
-      if (lastOperator === 'add') {
-        displayLine1.textContent = operate(add, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-      }
-      else if (lastOperator === 'subtract') {
-        displayLine1.textContent = operate(subtract, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-      }
-      else if (lastOperator === 'multiply') {
-        displayLine1.textContent = operate(multiply, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-      }
-      else if (lastOperator === 'divide') {
-        if (bufferOperand === "0") {
-          displayLine1.textContent = "";
-          displayLine2.textContent = 'Division by zero!!!';
-          break;
-        }
-        displayLine1.textContent = operate(divide, +lastOperand, +bufferOperand);
-        lastOperand = displayLine1.textContent;
-        bufferOperand = "";
-      }
-      lastOperator = "";
+    case '÷':
+      calculator.dispLine1 = operate(divide, operandA, operandB);
       break;
   }
+  calculator.result = calculator.dispLine1; // store previous result if operations chained
 };
+//---------------------------------------------------------------------------------
+const parseExpression = function(displayExpression) {
+  let splitChar1;
+  let splitChar2 = '=';
+  switch (calculator.currentOperator) {
+    case '+':
+      splitChar1 = '+';
+      break;
+    case '-':
+      splitChar1 = '-';
+      break;
+    case '✕':
+      splitChar1 = '✕';
+      break;
+    case '÷':
+      splitChar1 = '÷';
+      break;
+  }
+  let operandsA = displayExpression.split(splitChar1);
+  calculator.firstOperand = operandsA[0];
 
+  let operandsB = operandsA[1].split('=');
+  calculator.secondOperand = operandsB[0];
+}
+//---------------------------------------------------------------------------------
 operandButtons.forEach((operandButton) => {
-  operandButton.addEventListener('click', displayOperand);
+  operandButton.addEventListener('click', getInput);
 });
 
 operatorButtons.forEach((operatorButton) => {
   operatorButton.addEventListener('click', setOperator);
 });
+
+clearButton.addEventListener('click', clearDisplay);
+//---------------------------------------------------------------------------------
